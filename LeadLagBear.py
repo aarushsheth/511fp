e0aef4a5df4deace7f3df7daed770ef4b36e2d57
Kevin Javier Zvonarek <60452556+zvonarek@users.noreply.github.com>
2023-04-19 17:28:15 -0400

    final version for paper

--- LeadLagBear.py
+++ LeadLagBear.py
@@ -14,11 +14,6 @@ def sell_stock(portfolio, stock, amount, price):
 def portfolio_value(portfolio, stock_prices):
     return sum(shares * stock_prices[stock] for stock, shares in portfolio.items())
 
-def average_out_degree(adj_matrix):
-    G = nx.from_numpy_array(adj_matrix, create_using=nx.DiGraph)
-    out_degrees = dict(G.out_degree())
-    average_out_degrees = {k: v / len(G) for k, v in out_degrees.items()}
-    return average_out_degrees
 def find_all_leader_lagger_pairs(masked_array, stock_names):
     leader_lagger_pairs = {}
     for row in range(masked_array.shape[0]):
@@ -28,16 +23,18 @@ def find_all_leader_lagger_pairs(masked_array, stock_names):
                 lagger = stock_names[col]
                 leader_lagger_pairs[(leader, lagger)] = masked_array[row, col]
     return leader_lagger_pairs
+
 def create_directed_graph(leader_lagger_dict, stock_colors):
     G = nx.DiGraph()
     for leader, lagger in leader_lagger_dict.items():
         G.add_edge(lagger, leader)
-    pos = nx.spring_layout(G)
-    plt.figure(figsize=(10, 10))
+    pos = nx.spring_layout(G, k=0.5) 
+    plt.figure(figsize=(8, 8))
     for stock, color in stock_colors.items():
-        nx.draw_networkx_nodes(G, pos, nodelist=[stock], node_color=color, alpha=0.8)
-        nx.draw_networkx_labels(G, pos, labels={stock: stock}, font_size=12, font_color='black')
-    nx.draw_networkx_edges(G, pos, edgelist=G.edges(), arrowstyle='->', arrowsize=20, node_size=1000, alpha=0.5, width=2)
+        nx.draw_networkx_nodes(G, pos, nodelist=[stock], node_color=color, node_size=2500, alpha=1)
+        nx.draw_networkx_labels(G, pos, labels={stock: stock}, font_size=12, font_color='white', font_weight='bold')
+    nx.draw_networkx_edges(G, pos, edgelist=G.edges(), arrowstyle='->', arrowsize=30, node_size=1000,
+                        alpha=1, width=4, edge_color='black', connectionstyle='arc3,rad=0.2')
     plt.axis('off')
     plt.show()
 
@@ -58,13 +55,7 @@ row_indices, col_indices = np.unravel_index(indices_flattened, masked_array.shap
 leader_lagger_dict = {stock_names[row]: stock_names[col] for row, col in zip(row_indices, col_indices)}
 
 year_daily_data = loaded_daily_data[-250:, :, :]
-pseudo_adj_matrix = year_daily_data[0]
-avg_out_degrees = average_out_degree(pseudo_adj_matrix)
 stocks_to_include = set(leader_lagger_dict.keys()).union(set(leader_lagger_dict.values()))
-# Use for bull market
-# end_date = stockData.index[-251]
-# start_date = end_date - pd.Timedelta(days=249)
-# filtered_stock_data = stockData.loc[start_date:end_date, stocks_to_include]
 filtered_stock_data = stockData.loc[:, stocks_to_include].tail(250)
 n_laggers = len(leader_lagger_dict.values())
 initial_investment = 500000
@@ -82,9 +73,7 @@ for i in range(1, n_days):
     for leader, lagger in leader_lagger_dict.items():
         current_leader_price = filtered_stock_data[leader].iloc[i]
         highest_leader_prices[leader] = max(highest_leader_prices[leader], current_leader_price)
-#         Use for bull market
-#         if current_leader_price >= 1.0 * filtered_stock_data[leader].iloc[i - 1]:
-        if current_leader_price >= 1.08 * filtered_stock_data[leader].iloc[i - 1]:
+        if current_leader_price >= 1.075 * filtered_stock_data[leader].iloc[i - 1]:
             price = filtered_stock_data[lagger].iloc[i]
             cash_to_invest = remaining_cash / n_laggers
             buy_stock(portfolio, lagger, cash_to_invest, price)
@@ -107,8 +96,6 @@ portfolio_values = [portfolio_value(portfolio, filtered_stock_data.iloc[i]) + re
 
 ticker = "^GSPC"
 stock = yf.Ticker(ticker)
-# Use for bull market
-# historical_data = stock.history(start="2021-06-15", end="2022-04-15")
 historical_data = stock.history(start="2022-03-29", end="2023-03-15")
 historical_returns = historical_data['Open'].pct_change().dropna()
 resampled_historical_returns = np.interp(
@@ -122,13 +109,11 @@ percentage_historical_returns = 100 * cumulative_historical_returns
 percentage_portfolio_returns = 100 * np.array(cumulative_portfolio_returns)
 percentage_portfolio_returns -= percentage_portfolio_returns[0]
 
-tick_locations = np.linspace(0, len(portfolio_values) - 1, num=5, dtype=int)
+tick_locations = np.linspace(0, len(portfolio_values) - 1, num=4, dtype=int)
 tick_labels = [filtered_stock_data.index[i].strftime("%b %Y") for i in tick_locations]
 plt.plot(percentage_historical_returns, label="S&P 500 Returns")
 plt.plot(percentage_portfolio_returns, label="Portfolio Returns")
 plt.ylabel("Returns (%)")
-# Use for bull market
-# plt.title("Trailing Stop: 0.10 and Buy Threshold: 1.0")
 plt.title("Trailing Stop: 0.10 and Buy Threshold: 1.075")
 plt.legend()
 plt.xticks(tick_locations, tick_labels)
@@ -137,16 +122,3 @@ plt.show()
 stock_colors = {stock: 'red' for stock in leader_lagger_dict.keys()}
 stock_colors.update({stock: 'blue' for stock in leader_lagger_dict.values()})
 create_directed_graph(leader_lagger_dict, stock_colors)
-first_day_data = loaded_daily_data[0, :, :]
-masked_array_first_day = np.ma.masked_array(first_day_data, mask=np.eye(498, dtype=bool))
-first_day_leader_lagger_pairs = find_all_leader_lagger_pairs(masked_array_first_day, stock_names)
-filtered_first_day_leader_lagger_pairs = {
-    (leader, lagger): value
-    for (leader, lagger), value in first_day_leader_lagger_pairs.items()
-    if leader in stockData.columns and lagger in stockData.columns
-}
-first_day_stock_colors = {}
-for leader, lagger in filtered_first_day_leader_lagger_pairs.keys():
-    first_day_stock_colors[leader] = 'red'
-    first_day_stock_colors[lagger] = 'blue'
-create_directed_graph(filtered_first_day_leader_lagger_pairs, first_day_stock_colors)
